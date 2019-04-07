3eb40534f10ea0aa5302f441037816db454c816c
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-04-07 12:02:50 +0200

    Save to local storage

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import Buttons from "./Buttons";
 
-const LOCAL_STORAGE_KEY = 'value';
+const LOCAL_STORAGE_KEY = 'state';
 
 class Counter extends Component {
     state = {
         value: 0,
     };
+
+    componentDidUpdate() {
+        window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.state));
+    }
+
+    componentDidMount() {
+        const state = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+        this.setState({...JSON.parse(state)});
+    }
 
     inc = () => {
         this.setState({value: this.state.value + 1})
@@ -32,13 +41,13 @@ class Counter extends Component {
     render() {
         return (
             <div
-            style={{
-                width: '50%',
-                padding: '20px',
-                border: '1px solid #ddd',
-                margin: '0 auto',
+                style={{
+                    width: '50%',
+                    padding: '20px',
+                    border: '1px solid #ddd',
+                    margin: '0 auto',
 
-            }}
+                }}
             >
                 <h1
                     style={{
